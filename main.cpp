6184835513d0ae5e7f632eb629f092a03371ebb3
Dawid Zyluk <dawid@typoedition.com>
2022-05-04 22:43:11 +0200

    Add Merge()

--- main.cpp
+++ main.cpp
@@ -2,57 +2,58 @@
 
 using namespace std;
 
-void Display(int* arr, int n);
-void SelectionSort(int* arr, int n);
-void BubbleSort(int* arr, int n);
+void Display(int* A, int n);
+void SelectionSort(int* A, int n);
+void BubbleSort(int* A, int n);
 void BinaryInsertionSort(int* A, int n);
 void InsertionSort(int* A, int n);
+void Merge(int* A, int l, int mid, int h);
 
 int main()
 {
-	int tablica[] = { 6,-2,0,3,5,4,2,5,1 };
-	int n = size(tablica);
+	int A[] = { 2,5,8,12,3,6,7,10 };
+	int n = size(A);
 
-	Display(tablica, n);
-	BubbleSort(tablica, n);
-	Display(tablica, n);
+	Display(A, n);
+	Merge(A, 0, 3,7);
+	Display(A, n);
 }
 
-void Display(int* arr, int n)
+void Display(int* A, int n)
 {
 	cout << "[ ";
 	for (int i = 0; i < n; i++)
 	{
-		cout << arr[i];
+		cout << A[i];
 		if (i < n - 1) cout << ", ";
 	}
 	cout << " ]" << endl;
 }
 
-void SelectionSort(int* arr, int n)
+void SelectionSort(int* A, int n)
 {
 	for (int i = 0; i < n - 1; i++)
 	{
 		int min = i;
 		for (int j = i + 1; j < n; j++)
-			if (arr[j] < arr[min]) min = j;
-		int temp = arr[i];
-		arr[i] = arr[min];
-		arr[min] = temp;
+			if (A[j] < A[min]) min = j;
+		int temp = A[i];
+		A[i] = A[min];
+		A[min] = temp;
 	}
 }
 
-void BubbleSort(int* arr, int n)
+void BubbleSort(int* A, int n)
 {
 	for (int i = 0; i < n - 1; i++)
 	{
 		for (int j = i + 1; j < n; j++)
 		{
-			if (arr[i] > arr[j])
+			if (A[i] > A[j])
 			{
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
+				int temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
 			}
 		}
 	}
@@ -89,4 +90,20 @@ void InsertionSort(int* A, int n)
 		}
 		A[i - 1] = x;  
 	}
+}
+
+void Merge(int* A, int low, int mid, int high)
+{
+	int i = low;
+	int j = mid + 1;
+	int k = low;
+	int* B = new int[high+1];
+	while (i <= mid && j <= high)
+	{
+		if (A[i] < A[j]) B[k++] = A[i++];
+		else B[k++] = A[j++];
+	}
+	while (i <= mid) B[k++] = A[i++];
+	while (j <= high) B[k++] = A[j++];
+	for (int l = 0; l < high + 1; l++) A[l] = B[l];
 }
