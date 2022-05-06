22cdbd1ed4972a00e2e9d3c9f560e431bb54a346
Dawid Zyluk <dawid@typoedition.com>
2022-05-06 21:59:10 +0200

    Add Partition() and QuickSort()

--- main.cpp
+++ main.cpp
@@ -12,12 +12,15 @@ void InsertionSort(int* A, int n);
 void Merge(int* A, int low, int mid, int high);
 void IterativeMergeSort(int* A, int n);
 void RecursiveMergeSort(int* A, int low, int high);
+int Partition(int* A, int low, int high);
+void QuickSort(int* A, int low, int high);
 
 int main()
 {
-	int A[] = { 0,1,2,3,4,5,6,7,8 };
+	int A[] = { 5,9,1,8,2,7,3,6,4 };
 	int n = size(A);
-	SelectionSort(A, n);
+
+	Partition(A, 0, 8);
 	Display(A, n);
 }
 
@@ -145,5 +148,33 @@ void RecursiveMergeSort(int* A, int low, int high)
 		RecursiveMergeSort(A, low, mid);
 		RecursiveMergeSort(A, mid+1, high);
 		Merge(A, low, mid, high);
+	}
+}
+
+int Partition(int* A, int low, int high)
+{
+	int pivot = A[low];
+	int i = low + 1;
+	int j = high;
+
+	while (true)
+	{
+		while (A[i] <= pivot && i <= j) i++;
+		while (A[j] >= pivot && j >= i) j--;
+		if (j < i) break;
+		else Swap(A[i], A[j]);
+		Display(A, high + 1);
+	}
+	Swap(A[low], A[j]);
+	return j;
+}
+
+void QuickSort(int* A, int low, int high)
+{
+	if (low < high)
+	{
+		int p = Partition(A, low, high);
+		QuickSort(A, low, p - 1);
+		QuickSort(A, p + 1, high);
 	}
 }
